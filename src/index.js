33040f088016e258364c9b0e0d491e1fc7e30222
Sten Muchow <sten.muchow@gmail.com>
2019-01-25 11:09:52 +0000

    update to cra-2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -12,4 +12,5 @@ if (module.hot) {
     ReactDOM.render(<App />, document.getElementById('root'));
   });
 }
-registerServiceWorker();
+
+serviceWorker.unregister();
